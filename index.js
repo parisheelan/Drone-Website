c3e2288da348b8452d94da73638e3cf6d949fc5c
Parisheelan Negi <parisheelannegi@gmail.com>
2022-05-13 11:03:09 +0530

    .

--- index.js
+++ index.js
@@ -19,6 +19,25 @@ if (window.matchMedia("(prefers-color-scheme: light)").matches) {
   body.classList.remove("dark");
 }
 
-hamburger.addEventListener('click', () => {
-  nav.classList.toggle('show');
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("show");
+});
+
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]");
+
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
 });
